28c8e6b316883dd30248b480d8f537b96a7b0029
Ayamainstream <72498812+Ayamainstream@users.noreply.github.com>
2021-10-25 23:52:23 +0600

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,11 @@
+create table Users(
+  id INT PRIMARY KEY,
+  login VARCHAR(50),
+  password VARCHAR(50),
+  token text
+);
+insert into Users (id, login, password, token) values (1, 'jpattinson0', 'bVlQfImzm', '');
+insert into Users (id, login, password, token) values (2, 'lgroves1', 'lZuwCD', '');
+insert into Users (id, login, password, token) values (3, 'tbrain2', 'tOj6R1y', '');
+insert into Users (id, login, password, token) values (4, 'eleifer3', 'DjwqZDde', '');
+insert into Users (id, login, password, token) values (5, 'kmccart4', 'pAkhnfB', '');
